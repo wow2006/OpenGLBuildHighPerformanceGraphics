67c08c2652b7b6e077fa0fc228dd9042532c166a
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2019-12-31 18:45:47 +0200

    Add Module3 Chapter1 and Tutorial1

---
+++ Module3/Chapter1/Tutorial1/main.cpp
@@ -0,0 +1,50 @@
+// STL
+#include <cstdlib>
+// OpenGL
+#include <GLFW/glfw3.h>
+
+auto main() -> int {
+  if(!glfwInit()) {
+    return EXIT_FAILURE;
+  }
+
+  GLFWwindow *pWindow = glfwCreateWindow(640, 480, "Chapter 1: Simple GLFW Example", nullptr, nullptr);
+  if(pWindow == nullptr) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+  glfwMakeContextCurrent(pWindow);
+
+  while(!glfwWindowShouldClose(pWindow)) {
+    int width = 0, height = 0;
+    glfwGetFramebufferSize(pWindow, &width, &height);
+
+    const float ratio = static_cast<float>(width) / static_cast<float>(height);
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+
+    glfwSwapBuffers(pWindow);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(pWindow);
+  glfwTerminate();
+  return EXIT_SUCCESS;
+}
