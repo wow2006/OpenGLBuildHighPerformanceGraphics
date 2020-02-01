3bef18ceb250086ebf2079bcce46ba12f2c3a708
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2020-02-01 08:02:51 +0200

    formating

--- Module1/Chapter02/FreeCamera/main.cpp
+++ Module1/Chapter02/FreeCamera/main.cpp
@@ -1,6 +1,7 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 // STL
+#include <array>
 #include <iostream>
 #include <sstream>
 // OpenGL
@@ -41,10 +42,10 @@ struct Common {
 
   // mouse filtering support variables
   static constexpr float MOUSE_FILTER_WEIGHT = 0.75f;
-  static constexpr int MOUSE_HISTORY_BUFFER_SIZE = 10;
+  static constexpr auto MOUSE_HISTORY_BUFFER_SIZE = 10U;
 
   // mouse history buffer
-  glm::vec2 mouseHistory[MOUSE_HISTORY_BUFFER_SIZE];
+  std::array<glm::vec2, MOUSE_HISTORY_BUFFER_SIZE> mouseHistory;
 
   float mouseX = 0, mouseY = 0; // filtered mouse values
 
@@ -64,7 +65,7 @@ static Common *g_pCommon = nullptr;
 
 // mouse move filtering function
 void filterMouseMoves(float dx, float dy) {
-  for (int i = Common::MOUSE_HISTORY_BUFFER_SIZE - 1; i > 0; --i) {
+  for (auto i = Common::MOUSE_HISTORY_BUFFER_SIZE - 1; i > 0; --i) {
     g_pCommon->mouseHistory[i] = g_pCommon->mouseHistory[i - 1];
   }
 
@@ -77,7 +78,7 @@ void filterMouseMoves(float dx, float dy) {
   float currentWeight = 1.0f;
 
   // Filter the mouse.
-  for (int i = 0; i < Common::MOUSE_HISTORY_BUFFER_SIZE; ++i) {
+  for (auto i = 0U; i < Common::MOUSE_HISTORY_BUFFER_SIZE; ++i) {
     glm::vec2 tmp = g_pCommon->mouseHistory[i];
     averageX += tmp.x * currentWeight;
     averageY += tmp.y * currentWeight;
@@ -96,16 +97,17 @@ void OnMouseDown(int button, int s, int x, int y) {
     g_pCommon->oldY = y;
   }
 
-  if (button == GLUT_MIDDLE_BUTTON)
+  if (button == GLUT_MIDDLE_BUTTON) {
     g_pCommon->state = 0;
-  else
+  } else {
     g_pCommon->state = 1;
+  }
 }
 
 // mouse move handler
 void OnMouseMove(int x, int y) {
   if (g_pCommon->state == 0) {
-    g_pCommon->fov += (y - g_pCommon->oldY) / 5.0f;
+    g_pCommon->fov += static_cast<float>(y - g_pCommon->oldY) / 5.0f;
     g_pCommon->cam.SetupProjection(g_pCommon->fov, g_pCommon->cam.GetAspectRatio());
   } else {
     g_pCommon->rY += (y - g_pCommon->oldY) / 5.0f;
@@ -124,16 +126,25 @@ void OnMouseMove(int x, int y) {
   glutPostRedisplay();
 }
 
+namespace std {
+template<typename type, std::uint32_t w, std::uint32_t h>
+using array2D = std::array<std::array<type, w>, h>;
+} // namespace std
+
 // initialize OpenGL
 void OnInit() {
   GL_CHECK_ERRORS
+  constexpr GLubyte gTextureDim     = 128;
+  constexpr GLubyte gTextureHalfDim = gTextureDim/2;
   // generate the checker texture
-  GLubyte data[128][128] = {{0}};
-  for (int j = 0; j < 128; j++) {
-    for (int i = 0; i < 128; i++) {
-      data[i][j] = (i <= 64 && j <= 64) || (i > 64 && j > 64) ? 255 : 0;
+  std::array2D<GLubyte, gTextureDim, gTextureDim> data = {{{0}}};
+  for (GLubyte j = 0; j < gTextureDim; j++) {
+    for (GLubyte i = 0; i < gTextureDim; i++) {
+      data[i][j] = (i <= gTextureHalfDim && j <= gTextureHalfDim) ||
+                   (i > gTextureHalfDim && j > gTextureHalfDim) ? 255 : 0;
     }
   }
+
   // generate texture object
   glGenTextures(1, &g_pCommon->checkerTextureID);
   glActiveTexture(GL_TEXTURE0);
@@ -155,11 +166,11 @@ void OnInit() {
 
   // set mipmap base and max level
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 4);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL,  4);
 
   // allocate texture object
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 128, 128, 0, GL_RED, GL_UNSIGNED_BYTE,
-               data);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, gTextureDim, gTextureDim, 0, GL_RED, GL_UNSIGNED_BYTE,
+               data.data());
 
   // generate mipmaps
   glGenerateMipmap(GL_TEXTURE_2D);
@@ -178,12 +189,12 @@ void OnInit() {
   glm::vec3 look = glm::normalize(p);
 
   // rotate the camera for proper orientation
-  float yaw = glm::degrees(std::atan2(look.z, look.x) + static_cast<float>(M_PI));
-  float pitch = glm::degrees(std::asin(look.y));
+  const auto yaw   = glm::degrees(std::atan2(look.z, look.x) + static_cast<float>(M_PI));
+  const auto pitch = glm::degrees(std::asin(look.y));
   g_pCommon->rX = yaw;
   g_pCommon->rY = pitch;
   if (g_pCommon->useFiltering) {
-    for (int i = 0; i < Common::MOUSE_HISTORY_BUFFER_SIZE; ++i) {
+    for (auto i = 0u; i < Common::MOUSE_HISTORY_BUFFER_SIZE; ++i) {
       g_pCommon->mouseHistory[i] = glm::vec2(g_pCommon->rX, g_pCommon->rY);
     }
   }
@@ -221,16 +232,16 @@ void OnIdle() {
 void OnRender() {
   // timing related calcualtion
   g_pCommon->last_time    = g_pCommon->current_time;
-  g_pCommon->current_time = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+  g_pCommon->current_time = static_cast<float>(glutGet(GLUT_ELAPSED_TIME)) / 1000.0f;
   g_pCommon->dt           = g_pCommon->current_time - g_pCommon->last_time;
 
   // clear color buffer and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   // set the camera transformation
-  glm::mat4 MV  = g_pCommon->cam.GetViewMatrix();
-  glm::mat4 P   = g_pCommon->cam.GetProjectionMatrix();
-  glm::mat4 MVP = P * MV;
+  const auto MV  = g_pCommon->cam.GetViewMatrix();
+  const auto P   = g_pCommon->cam.GetProjectionMatrix();
+  const auto MVP = P * MV;
 
   // render the chekered plane
   g_pCommon->checker_plane->Render(glm::value_ptr(MVP));
@@ -291,6 +302,7 @@ auto main(int argc, char **argv) -> int {
     }
   }
   err = glGetError(); // this is to ignore INVALID ENUM error 1282
+  (void)err;
   GL_CHECK_ERRORS
 
   // print information on screen
